545b6719b41e53df9b6ccad5df06cfdb62b9a158
RandyLambert <1249604462@qq.com>
2019-07-26 22:18:03 +0800

    hhhh

--- member/孙首勋/week2/shell/myshell.c
+++ member/孙首勋/week2/shell/myshell.c
@@ -14,57 +14,65 @@
 #define BUFFSIZE 64
 #define normal 0        // 一般的命令
 #define out_redirect 1  // 输出重定向
+#define outtwo_redirect 4 //>>
 #define in_redirect 2   // 输入重定向
 #define have_pipe 3     // 命令中有管道
+
 void print_prompt();    // 打印提示符
-/* void get_input(char *); // 读取用户输入的命令 */
 // 二维数组作为函数参数，第1维长度可以不指定，但必须指定第2维长度
 // 也可使用指向含8个元素一维数组的指针，char (*a)[8]
 void explain_input(char *, int *, char a[][256]); // 解析输入的命令
 void do_cmd(int, char a[][256]);  // 执行命令
 int find_command(char *);       // 查找命令中的可执行程序
+char aaa[256];
+char past[256];
 
 int main(int argc, char *argv[])
 {
-    /* signal(SIGINT,SIG_IGN); */
+    signal(SIGINT,SIG_IGN);
     char arglist[100][256];
     int argcount = 0;
     char **arg = NULL;
     char *buf = NULL;
-    buf = (char *)malloc(256);
+    buf = (char*)malloc(256);
 
-    if(buf == NULL){
-        perror("malloc error\n");
-        exit(-1);
-    }
+
 
     while(1)
     {
         memset(buf,0,256);
-        char *str = readline("\033[47;33mSSX:\033[0m");
         print_prompt(); // 打印提示符
-        
+        char *str = readline("\033[37mSSX)\033[0m$ ");
+        add_history(str);
         strcpy(buf,str);
-        add_history(buf);
-        if(strcmp(buf,"exit\n")==0 || strcmp(buf,"long out\n") == 0){
+        
+        if(str!=NULL){
+            free(str);
+            str = NULL;
+        }
+        if(strcmp(buf,"exit")==0 || strcmp(buf,"logout") == 0){
             break;
         }
 
-        if(strcmp(buf,"\n")==0) continue;
+        if(strcmp(buf,"\0")==0) continue;
 
         for(int i=0; i<100; i++)     // 初始化存放命令及其参数的数组
             arglist[i][0] = '\0';
+
             argcount = 0;   // 计数，命令中词汇数量
             explain_input(buf, &argcount, arglist); // 解析命令
             do_cmd(argcount, arglist);      // 执行命令
+            if(str != NULL){
+                free(str);
+                str = NULL;
         }
-
-    if(buf != NULL){
-        free(buf);
-        /* free(str); */
-        buf = NULL;
-        /* str = NULL; */
     }
+    
+    if(buf!=NULL){
+        free(buf);
+        buf = NULL;
+    }
+
     exit(0);
 
 }
@@ -72,41 +80,17 @@ int main(int argc, char *argv[])
 
 void print_prompt()
 {
-    char *a = (char *)malloc(256);
-    getcwd(a,256);
-    printf("[0m\033[47;34m%s\033[0m$",a);
-    /* printf("RandyLambert%s$",a); */
+    getcwd(aaa,256);
+    printf("\033[34m%s(from \033[0m",aaa);
     //      fflush(stdout); // fflush 冲洗stdout
-    free(a);
 }
 
-/*void get_input(char *buf)
-{
-    int len = 0;
-    int ch;
-        
-    ch = getchar();
-    while(len < 256 && ch != '\n'){
-        buf[len++] = ch;
-        ch = getchar();
-    }
-
-    if(len >= 256){
-        printf("too long \n");
-        exit(-1);
-    }
-    else{
-        buf[len] = '\n';
-        len++;
-        buf[len] = '\0';
-    }   
-}
-*/
 
 // 解析buf中的命令，结果存入arglist中，命令及其参数个数为argcount
 // 如，"ls -l"命令，则arglist[0]、arglist[1]分别为ls、-l
 void explain_input(char *buf, int *argcount, char arglist[100][256])
 {
+    /* printf("%s",buf); */
     char *p = buf;
     char *q = buf;
     int number = 0;
@@ -155,14 +139,13 @@ void do_cmd(int argcount, char arglist[100][256])
     for(int i = 0;i < argcount;i++){
         arg[i] = (char *)arglist[i];
         /* printf("%s\n",arg[i]); */
-        /* if(strncmp("ls",arg[i],2)==0){ */
-
-        /* } */
     }
+    
     arg[argcount] = NULL;
-    if(strcmp(arg[0],"ls\n") == 0){
-        strcpy(arg[0],"ls --color=auto\n");
-    }
+    /* printf("%s\n",arg[0]); */
+
+
+
 
     //查看命令行是否有后台运行符
 
@@ -180,45 +163,15 @@ void do_cmd(int argcount, char arglist[100][256])
             }
         }
     }
-    // 查看命令行是否有后台运行符
-    for(i=0; i<argcount; i++)
-    {
-        if(strncmp(arg[i], "&", 1) == 0)        // strncmp
-        {       // 后台运行符必须在命令的末尾，否则命令格式错误
-            if(i == argcount-1)
-            {
-                background = 1;
-                arg[argcount-1] = NULL;
-                break;
-            }
-            else
-            {
-                printf("ERROR: wrong command about backgrount\n");
-                return;
-            }
-        }
-    }
 
     for(i=0; arg[i]!=NULL; i++)
     {
         if(strcmp(arg[i], ">") == 0)
         {
-            if(strcmp(arg[i+1],">") != 0){
-                flag++;
-                how = out_redirect;
-                if(arg[i+1] == NULL)    // 输出重定向符在最后面
-                    flag++; // 使flag大于1，告知命令格式错误
-            }
-
-
-            else if(strcmp(arg[i+1],">") == 0){
-                cnt++;
-                flag++;
-                how = out_redirect;
-                if(arg[i+2] == NULL)
-                    flag++;
-            }
-            
+            flag++;
+            how = out_redirect;
+            if(arg[i+1] == NULL)    // 输出重定向符在最后面
+                flag++; // 使flag大于1，告知命令格式错误
         }
         if(strcmp(arg[i], "<") == 0)
         {
@@ -236,6 +189,14 @@ void do_cmd(int argcount, char arglist[100][256])
             if(i == 0)      // 管道符在最前面
                 flag++;         //同上
         }
+        if(strcmp(arg[i], ">>") == 0)
+        {
+            flag++;
+            how = outtwo_redirect;
+            if(arg[i+1] == NULL)    // 输出重定向符在最后面
+                flag++; // 使flag大于1，告知命令格式错误
+        }
+        
     }
     // flag大于1，说明同时含有>,<,|中的两个或以上，本程序不支持
     // 或者命令格式错误
@@ -244,6 +205,58 @@ void do_cmd(int argcount, char arglist[100][256])
         return;
     }
 
+    if(strcmp(arg[0],"ls") == 0){
+        strcpy(arglist[argcount],"--color");
+        arg[argcount] = (char *)arglist[argcount];
+
+        argcount = argcount + 1;
+        arg[argcount] = NULL;
+    }
+    else{
+        arg[argcount] = NULL;
+    }
+    
+    if(strncmp(arg[0],"cd",2) == 0){
+        
+        if(argcount == 1){
+            strcpy(past,aaa);
+            chdir("/home/sunxiaochuan/");
+            return ;
+        }
+        if(strncmp(arg[1],"~",1) == 0 || argcount == 1){
+            strcpy(past,aaa);
+            chdir("/home/sunxiaochuan/");
+            return ;
+        }
+        else if(strncmp(arg[1],"-",1) == 0){
+            if(past[0] == '\0' && past[1] == '\0'){
+                printf("ERROR: wrong command about cd\n");
+                return ;
+            }
+            else{
+                if(chdir(past) == -1){
+                    printf("bash: cd: %s: 没有那个文件或目录\n",arg[1]);
+                }
+            }
+        }
+        else if(argcount == 2){
+            strcpy(past,aaa);
+            if(chdir(arg[1]) == -1){
+                printf("bash: cd: %s: 没有那个文件或目录\n",arg[1]);
+            }
+        }
+        return ;
+    }
+
+    if(how == outtwo_redirect){
+        for(i = 0; arg[i]!=NULL;i++){
+            if(strcmp(arg[i], ">>") == 0)
+            {
+                file = arg[i+1]; // 获取输出重定向的文件名
+                arg[i] = NULL;
+            }
+        }
+    }
     if(how == out_redirect) // 命令中只含有一个输出重定向符
     {
         for(i=0; arg[i]!=NULL; i++)
@@ -284,6 +297,7 @@ void do_cmd(int argcount, char arglist[100][256])
     if((pid = fork()) < 0){
         printf("fork error\n");
     }
+
 
     switch(how){
     case 0:
@@ -334,7 +348,7 @@ void do_cmd(int argcount, char arglist[100][256])
             int status2;
             int fd2;
 
-            if((pid2 == fork()) < 0){
+            if((pid2 = fork()) < 0){
                 printf("fork2 error\n");
                 return ;
             }
@@ -354,15 +368,30 @@ void do_cmd(int argcount, char arglist[100][256])
             }
 
             if(!(find_command(argnext[0]))){
-                printf("%s: command not found\n", arg[0]);
+                printf("%s: command not found\n", argnext[0]);
                 exit(0);
             }
             fd2 = open("/tmp/youdonotknowfile",O_RDONLY);
             dup2(fd2,0);
             execvp(argnext[0],argnext);
-
             if(remove("/tmp/youdonotknowfile"))
                 printf("remove error\n");
+            exit(0);
+        }
+        break;
+    case 4:
+        if(pid == 0)//>>
+        {
+            if(!find_command(arg[0]))
+            {
+                printf("%s: command not found\n", arg[0]);
+                exit(0);
+            }
+            // 打开或新建输出重定向的文件
+            int fd = open(file, O_RDWR | O_APPEND | O_CREAT , 0644);
+            // 将标准输出复制到打开的文件描述符，即用文件描述符替换标准输出
+            dup2(fd, 1);    // dup2(int oldfd, int newfd)
+            execvp(arg[0], arg);    // execvp
             exit(0);
         }
         break;
@@ -390,8 +419,7 @@ int find_command(char *command){
     struct dirent *dirp;
     char *path[] = {"./", "/bin", "/usr/bin", NULL};
     // 当输入命令"./build"时，将build命令与目录中的build文件进行匹配
-
-    if(strncmp(command,"./",2) == 2){
+    if(strncmp(command,"./",2) == 0){
         command = command + 2;
     }
     int i = 0;
