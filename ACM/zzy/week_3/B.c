aab2590bd563c3c828e6ea56b9358af23e053740
XU-zzy <1245215743@qq.com>
2019-08-07 00:22:42 +0800

    by zzy

--- ACM/zzy/week_3/B.c
+++ ACM/zzy/week_3/B.c
@@ -1,42 +1,59 @@
 #include<stdio.h>
 #include<string.h>
 #include<math.h>
-int hang[11];
-int count;
-int n;
+int book[8][8];     //标记
+int right[92][8];   //正确组数
+int count = 0;      //控制给第几组赋值
 void dfs(int lie){
-    if (lie == n){//循环到第 n 个，数目+1
+    //超过界限，返回
+    if(lie == 8){
         count++;
         return;
     }
-    int i,k;
-    for(i = 0;i < n;i++){//判断第 lie 列，第 i 个
-        for(k = 0;k < lie;k++){//在已经记录的 hang 中，第 k 列 第 hang[k] 行
-            if(hang[k] == i || (hang[k] - i) == (k - lie) || (hang[k] - i) == (lie - k))
-                break;
+    int i;
+    for(i = 0;i < 8;i++){
+        int k;
+        if(book[i][lie] == -1){     //判断后放棋子
+            book[i][lie] = lie;
+            //放入right
+            for(k = count;k < 92;k++){
+                right[k][lie] = i + 1;
+            }
+            //标记棋子可覆盖的区域
+            for(int m = 0;m < 8;m++){
+                for(int n = 0;n < 8;n++){
+                    if((m == i || n == lie || abs(m - i) == abs(n - lie)) && (book[m][n] == -1)){
+                        book[m][n] = lie;
+                   }
+                }
+            }
+            //向下查找
+            dfs(lie+1);
+            //地图回溯
+            for(int a = 0;a < 8;a++){
+                for(int b = 0;b < 8;b++){
+                    if(book[a][b] == lie){
+                        book[a][b] = -1;
+                    }
+                }
+            }
         }
-        //没有重复的，录入并尝试下一个
-        if(k == lie){
-            hang[lie] = i;
-            dfs(lie + 1);
-        }  
     }
 }
 
 int main(){
-    int a[11];
-    for(int i = 0;i < 11;i++){
-        count = 0;
-        n = i;
-        dfs(0);
-        a[i] = count;
-    }
-    while(1){  
+    int N;
+    scanf("%d",&N);
+    memset(book,-1,sizeof(book));
+    memset(right,-1,sizeof(right));
+
+    dfs(0);
+    int n;
+    for(int i = 0;i < N;i++){
         scanf("%d",&n);
-        if(n == 0)
-            break;
-        /* memset(hang,-1,sizeof(hang)); */
-        printf("%d\n",a[n]);
+        for(int j = 0;j < 8;j++){
+            printf("%d",right[n-1][j]);
+        }
+        printf("\n");
     }
-    return 0;
 }
